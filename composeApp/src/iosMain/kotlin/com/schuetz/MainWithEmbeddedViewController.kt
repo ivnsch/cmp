dea839959429a62afed8654afe2617d62a7468df
Ivan Schuetz <ivanschuetz@gmail.com>
2025-05-18 06:48:12 +0200

    Remove unnecessary annotation

--- composeApp/src/iosMain/kotlin/com/schuetz/MainWithEmbeddedViewController.kt
+++ composeApp/src/iosMain/kotlin/com/schuetz/MainWithEmbeddedViewController.kt
@@ -15,10 +15,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.UIKitInteropProperties
 import androidx.compose.ui.viewinterop.UIKitViewController
 import androidx.compose.ui.window.ComposeUIViewController
-import kotlinx.cinterop.ExperimentalForeignApi
 import platform.UIKit.UIViewController
 
-@OptIn(ExperimentalForeignApi::class)
 fun ComposeEntryPointWithUIViewController(
     createUIViewController: (Double) -> UIViewController
 ): UIViewController {
